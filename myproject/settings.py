132f6a75c8f206b7c16c4010c25bedec4eaff8aa
Anshu-vishwakarma <89008910+Anshu-vishwakarma@users.noreply.github.com>
2022-11-16 07:14:34 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = 'static/'
-MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR/'media'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATICFILES_DIRS=(
-    BASE_DIR/'static',
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
