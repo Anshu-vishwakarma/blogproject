3bfdc041e67f4f88e37e4f4869101d5c28d842eb
Anshu-vishwakarma <89008910+Anshu-vishwakarma@users.noreply.github.com>
2022-11-16 09:53:40 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,9 +21,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login,name='login'),
-    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
-    path('social-auth/',include('social_django.urls',namespace='social')),
+#     path('login/', views.login,name='login'),
+#     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
+#     path('social-auth/',include('social_django.urls',namespace='social')),
     # path('oauth/',include('social_django.urls',namespace='social2')),
     path('',views.home,name='home'),
     # path('upimage/',include('img.urls')),
